89fd798fdc8ce355448658c7e19f9b2043cb8995
Adiiiijain04 <jain.delhi1008@gmail.com>
2024-10-19 22:23:29 +0530

    Added a lot of pases , styles ,ahhhhhhhhhhhh

--- src/app.py
+++ src/app.py
@@ -8,27 +8,52 @@ from oauthlib.oauth2.rfc6749.errors import TokenExpiredError
 from requests.models import Response
 
 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 from os import environ
 from datetime import datetime
 import json
 
+load_dotenv()
 app = Flask(__name__)
 api = Api(app)
 
+# app.secret_key = environ['SECRET_KEY']
+# app.config['SESSION_TYPE'] = ''
+
+environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' # ONLY ON LOCAL ENV
+blueprint = make_google_blueprint(
+    client_id     = environ['GOOGLE_CLIENT_ID'],
+    client_secret = environ['GOOGLE_CLIENT_SECRET'],
+    scope         = ['email','profile'],
+    offline       = True,
+    redirect_to   = 'google_auth'
+)
+app.register_blueprint(blueprint, url_prefix='/login')
+
+
+
+@app.route('/google_auth')
+def google_auth():
+    # get email and name from google
+    if not google.authorized:
+        return redirect(url_for('google.login'))
+    try:
+        resp = google.get('/oauth2/v2/userinfo')
+        assert resp.ok, resp.text
+        email = resp.json()['email']
+        name = resp.json()['name']
+        session['email'] = email
+        session['name'] = name
+        return redirect(url_for('dashboard'))
+    except TokenExpiredError:
+        return redirect(url_for('google.login'))
+    
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/community')
-def community():
-    return render_template('community.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
 
 @app.route('/about')
 def about():
@@ -53,6 +78,46 @@ def homecare():
 @app.route('/medicalcare')
 def medicalcare():
     return render_template('medicalcare.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+
+
+@app.route('/community')
+def community():
+    return render_template('community.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')
+
+@app.route('/fitness')
+def fitness():
+    return render_template('fitness.html')
+
+@app.route('/health')
+def health():
+    return render_template('health.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/testimonials')
+def testimonials():
+    return render_template('testimonials.html')
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
