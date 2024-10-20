6dd4fa93958007c31f0630f75e22b2d2b2ba1b37
Ridhim15 <ridhimg15@gmail.com>
2024-10-20 06:10:15 +0530

    added a bit of database

--- src/app.py
+++ src/app.py
@@ -12,27 +12,46 @@ from datetime import datetime
 import json
 import mysql.connector
 
-
 load_dotenv()
 app = Flask(__name__)
 api = Api(app)
 app.secret_key = environ['SECRET_KEY']
-
-# connection=mysql.connector.connect(host='localhost, user='root', password='', database='Parvaah')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databases.db'
 db = SQLAlchemy(app)
+# -------------------- Session config --------------------
+app.config['SESSION_TYPE']             = 'sqlalchemy'
+app.config['SESSION_SQLALCHEMY']       = db
+app.config['SESSION_SQLALCHEMY_TABLE'] = 'sessions'
+Session(app)
+# connection=mysql.connector.connect(host='localhost, user='root', password='', database='Parvaah')
+
+
+
 
 class Elderly(db.Model):
+    id       = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    email    = db.Column(db.String(50), unique=True, nullable=False)
+    image_url= db.Column(db.String(200))
+    name     = db.Column(db.String(100))
+    age      = db.Column(db.Integer)
+    gid      = db.Column(db.Integer)
+    # guardian = db.relationship(Guardian, backref=db.backref('elderly', lazy=True))
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+class Guardian(db.Model):
     id       = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
     image_url= db.Column(db.String(200), nullable=False)
     name     = db.Column(db.String(100), nullable=False)
     age      = db.Column(db.Integer, nullable=False)
-    g_id     = db.Column(db.Integer, db.ForeignKey(Guardian.id), nullable=False)
-    guardian = db.relationship(Guardian, backref=db.backref('elderly', lazy=True))
-
-
+    eid      = db.Column(db.Integer , nullable=False)
+    # elderly = db.relationship(Elderly, backref=db.backref('guardian', lazy=True))
 
 environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' # ONLY ON LOCAL ENV
@@ -48,10 +67,13 @@ app.register_blueprint(blueprint, url_prefix='/login')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-
-
+    logged_in = 'username' in session
+    if logged_in:
+        username = session['username']
+        current_user = Elderly.query.filter_by(username=username).first()
+        return render_template('index.html', logged_in=logged_in, current_user=current_user.username)
+    else:
+        return render_template('index.html', logged_in=logged_in)
 @app.route('/<user>')
 def user(user):
     return render_template('login.html', user=user)
@@ -64,43 +86,42 @@ def login():
 
         elderly = Elderly.query.filter_by(username=username, password=password).first()
 
-        if ngo:
-            session['username'] = ngo.username
-            session['user_id'] = ngo.id
-            return redirect(url_for('ngo', id=ngo.id))
+        if elderly:
+            session['username'] = elderly.username
+            session['user_id'] = elderly.id
+            return redirect(url_for('index'))
         else:
-            flash('Invalid username or password', 'error')
-
+            return 'Invalid username or password'
     return render_template('login.html')
 
-@app.route('/logout', methods=['POST'])
-def logout():
-    session.pop('username', None)
-    session.pop('user_id', None)
-    flash("You have been logged out.", "info")
-    return redirect(url_for('login'))
+# @app.route('/logout', methods=['POST'])
+# def logout():
+#     session.pop('username', None)
+#     session.pop('user_id', None)
+#     flash("You have been logged out.", "info")
+#     return redirect(url_for('login'))
 
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        phone = request.form['phone']
-        address = request.form['address']
-        email = request.form['email']
+        email    = request.form['email']
 
-        existing_user = NGO.query.filter_by(username=username).first()
-        if existing_user:
-            flash('Username already exists.', 'error')
-            return redirect(url_for('register'))
-        new_user = NGO(username=username, password=password, phone=phone, address=address, email=email)
-        db.session.add(new_user)
-        db.session.commit()
-        flash('Registered Successfully!', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html')
+        user = Elderly.query.filter_by(username=username).first()
+        if user:
+            return make_response('User already exists', 400)
+        else:
+            session['username'] = username
+            session['email'] = email
+            new_user = Elderly(username=username, password=password, email=email)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('index'))
+    return make_response('Invalid request method', 405)
+        
 @app.route('/google_auth')
 def google_auth():
     # get email and name from google
@@ -154,4 +175,6 @@ def community():
 
 
 if __name__ == '__main__':
+    with app.app_context(): db.create_all()
     app.run(debug=True)
+    
